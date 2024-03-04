76987dcdf7536a8ec188aa237119c28070be7859
Jaxku <52820208+Jaxku@users.noreply.github.com>
2024-03-05 11:52:48 +1300

    Orig Commit

---
+++ OOP_1.py
@@ -0,0 +1,19 @@
+class dog:
+    def __init__(selfself, name, age , colour_): # <etjpd ca;;ed wjemever we create a mew dog instance)
+    self.name = name
+    self.age = age
+    self.colour = colour_
+
+    def print_deatis(self):
+        print(f"Name: {self.name}, Age: {self.age}, Colour: {self.colour}")
+
+        def change_age(self, age):
+            self.age = age
+
+
+
+# Creating differnt dog objects
+dog1 = Dog("Spot", 5, "Brown")
+dog2 = Dog("jazz", 7, "Black")
+
+
---
+++ StudentClass_OOP_2.py
@@ -0,0 +1,9 @@
+# Setting up the sutdent class
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade
